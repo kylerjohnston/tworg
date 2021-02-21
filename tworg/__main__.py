091307d7576195292fe43676890e562fc797a0f3
Kyle Johnston <kylerjohnston@gmail.com>
2021-02-21 10:55:48 -0500

    Use poetry; break cli into module

--- tworg/__main__.py
+++ tworg/__main__.py
@@ -1,27 +1,5 @@
 """ Converts .tid Tiddlywiki files into Org-Mode files """
-import argparse
-import os
-
-from .tworg import Convertor
+from .console import run
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('tiddler', type=str, nargs='+',
-                        help='Tiddlers to convert')
-    parser.add_argument('-o', '--output', type=str, nargs=1, required=False,
-                        default='./', help='Directory to write org files to')
-    args = parser.parse_args()
-
-    for fp in args.tiddler:
-        print(fp)
-        # Ignore system tiddlers
-        if not fp.split('/')[-1].startswith('$_'):
-            c = Convertor()
-            with open(fp, 'r') as f:
-                c.load(f)
-            if c.metadata['title']:
-                with open(os.path.join(
-                        args.output[0],
-                        c.metadata['title'].replace('/', '_') + '.org'),
-                          'w') as f:
-                    print(c, file=f)
+    run()
